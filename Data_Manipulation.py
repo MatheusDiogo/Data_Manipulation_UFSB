f16f21d69118e6c0b726aa8e81b84035ba8c2052
Matheus Diôgo <matheusdiogoeng@gmail.com>
2023-07-28 02:43:50 -0300

    Numero de linhas Dinamico
    Identificação de numero de linhas até repetição de forma dinâmica sem input

--- Data_Manipulation.py
+++ Data_Manipulation.py
@@ -8,22 +8,22 @@ import pandas as pd
 import re
 
 ##################################################################################
-#                         Inicializando variaveis                                #
-##################################################################################
-NumR = input("Digite o número de linhas até a repetição dos dados: "); #Numero de linhas para repetição
-lstIteracao = [] # Variavel do tipo lista que ira conter os conteudos dos blocos de 17 linhas na iteracao
-listsupervisorio = [] # Variavel do tipo lista que ira conter o conteudo do arquivo .csv
-newlistsupervisorio = [] # Variavel do tipo lista que ira receber a lista com o conteudo sem caracteres indesejados
-supervisoriofinal = pd.DataFrame() # Variavel do tipo data frame que ira conter a formatacao final desejada
-
-##################################################################################
 #                         Abertura do Arquivo                                    #
 ##################################################################################
 
 abrir = input("Digite o nome do arquivo para tratamento de dados: ") # variavel que guarda o caminho do arquivo
 filename = abrir+".xlsx" #nome do arquivo de saida
 
-abre_Arquivo = pd.read_csv(abrir,encoding='ISO-8859-1', delimiter = ';') #variavel do tipo data frame que guardara o arquivo
+abre_Arquivo = pd.read_csv(abrir, encoding='ISO-8859-1', delimiter = ';') #variavel do tipo data frame que guardara o arquivo
+
+##################################################################################
+#                         Inicializando variaveis                                #
+##################################################################################
+NumR = abre_Arquivo['TAG'].eq('Descrição').idxmax() #Numero de linhas para repetição
+lstIteracao = [] # Variavel do tipo lista que ira conter os conteudos dos blocos de 17 linhas na iteracao
+listsupervisorio = [] # Variavel do tipo lista que ira conter o conteudo do arquivo .csv
+newlistsupervisorio = [] # Variavel do tipo lista que ira receber a lista com o conteudo sem caracteres indesejados
+supervisoriofinal = pd.DataFrame() # Variavel do tipo data frame que ira conter a formatacao final desejada
 
 ##################################################################################
 #                         Formatacao do Arquivo                                  #
